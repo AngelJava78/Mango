62e6fe3e2eb53deb7fe50629139d5da4bef9170f
AngelJava78 <angel_java@hotmail.com>
2022-09-16 18:48:36 -0500

    Product API implemented

---
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,112 @@
+﻿using Mango.Services.ProductAPI.Models.Dtos;
+using Mango.Services.ProductAPI.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ProductAPI.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductAPIController : ControllerBase
+    {
+        protected ResponseDto response;
+        readonly IProductRepository repository;
+        public ProductAPIController(IProductRepository repository)
+        {
+            response = new ResponseDto
+            {
+                IsSuccess = false,
+                ErrorMessages = new List<string>(),
+                DisplayMessage = string.Empty,
+                Result = null
+            };
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            try
+            {
+                var products = await repository.GetProducts();
+                response.IsSuccess = true;
+                response.Result = products;
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.ErrorMessages = new List<string>() { ex.Message };
+            }
+            return response;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<object> Get(int id)
+        {
+            try
+            {
+                var product = await repository.GetProductById(id);
+                response.IsSuccess = true;
+                response.Result = product;
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.ErrorMessages = new List<string>() { ex.Message };
+            }
+            return response;
+        }
+
+        [HttpPost]
+        public async Task<object> Post([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                var product = await repository.CreateUpdateProduct(productDto);
+                response.IsSuccess = true;
+                response.Result = product;
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.ErrorMessages = new List<string>() { ex.Message };
+            }
+            return response;
+        }
+
+        [HttpPut]
+        public async Task<object> Put([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                var product = await repository.CreateUpdateProduct(productDto);
+                response.IsSuccess = true;
+                response.Result = product;
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.ErrorMessages = new List<string>() { ex.Message };
+            }
+            return response;
+        }
+
+        [HttpDelete]
+        public async Task<object> Delete(int id)
+        {
+            try
+            {
+                var isSuccess= await repository.DeleteProduct(id);
+                response.IsSuccess = true;
+                response.Result = isSuccess;
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.ErrorMessages = new List<string>() { ex.Message };
+            }
+            return response;
+        }
+    }
+}
---
+++ Mango.Services.ProductAPI/Repository/ProductRepository.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using Mango.Services.ProductAPI.DbContexts;
+using Mango.Services.ProductAPI.Models;
+using Mango.Services.ProductAPI.Models.Dtos;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductAPI.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        readonly ApplicationDbContext db;
+        readonly IMapper mapper;
+        public ProductRepository(ApplicationDbContext db, IMapper mapper)
+        {
+            this.db = db;
+            this.mapper = mapper;
+        }
+
+        public async Task<ProductDto> CreateUpdateProduct(ProductDto productDto)
+        {
+            var product = mapper.Map<ProductDto, Product>(productDto);
+            if(product.ProductId >0)
+            {
+                db.Products.Update(product);
+                
+            }
+            else
+            {
+                db.Products.Add(product);
+            }
+            await db.SaveChangesAsync();
+            return mapper.Map<Product,ProductDto>(product);
+        }
+
+        public async Task<bool> DeleteProduct(int productId)
+        {
+            var result = false;
+            try
+            {
+                var product = await db.Products.FirstOrDefaultAsync(x => x.ProductId == productId);
+                if (product == null)
+                {
+                    result = false;
+                }
+                else
+                {
+                    db.Products.Remove(product);
+                    await db.SaveChangesAsync();
+                    result = true;
+                }
+            }
+            catch (Exception)
+            {
+                result=  false;
+            }
+            return result;
+        }
+
+        public async Task<ProductDto> GetProductById(int productId)
+        {
+            var product = await db.Products.Where(x => x.ProductId == productId).FirstOrDefaultAsync();
+            return mapper.Map<ProductDto>(product);
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetProducts()
+        {
+            var products = await db.Products.ToListAsync();
+            return mapper.Map<List<ProductDto>>(products);
+        }
+    }
+}
